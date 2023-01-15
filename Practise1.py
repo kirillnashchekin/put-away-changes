76894e4964251820656b53daf33617a8ea3d9497
Kiriill Nashchekin <kirillnashchekin@gmail.com>
2023-01-15 10:44:35 +0300

    initial

---
+++ Practise1.py
@@ -0,0 +1,6 @@
+print('Здравствуйте')
+
+for number in range (1, 10):
+  for digit in range(1, 10):
+    print( number * digit, end =' \t ') 
+  print()
---
+++ calcute.py
@@ -0,0 +1,44 @@
+print('Здравствуйте.')
+
+start_phrase =' '
+summ = 0
+
+x = input('Выберите операцию:  ')
+numbers = int(input('Сколько будет чисел?: '))
+
+for number in range(1, numbers + 1):
+  print (' Введите',number,'- е число: ',end =' ')
+  number_1=input()
+  
+  if number == 1:
+    start_phrase = number_1 
+    summ = int(number_1)
+
+  elif number >= 2 and x == '+':  
+    start_phrase += x
+    start_phrase += number_1
+    summ += int(number_1)
+
+  elif number >= 2 and x == '-':
+    start_phrase += x  
+    start_phrase += number_1
+    summ -= int(number_1)
+
+  elif number >= 2 and x == '*':
+    start_phrase += x  
+    start_phrase += number_1
+    summ *= int(number_1)
+
+  elif number >= 2 and x == '//':
+    start_phrase += x  
+    start_phrase += number_1
+    summ //= int(number_1)
+
+print( start_phrase,'=',summ)
+
+
+
+
+
+ 
+
