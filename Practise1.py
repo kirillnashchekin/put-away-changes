9babe8c52e943467c9b07f4c82c07290219b7084
Kiriill Nashchekin <kirillnashchekin@gmail.com>
2023-01-16 21:03:15 +0300

    last changes

--- Practise1.py
+++ Practise1.py
@@ -1,7 +1,7 @@
-print('Здравствуйте,  мои коллеги.')
+print('Здравствуйте, друзья.')
 
 for number in range (1, 10):
   for digit in range(1, 10):
     print( number * digit, end =' \t ') 
   print()
-  print('Безконца.')
+  print(конец)
