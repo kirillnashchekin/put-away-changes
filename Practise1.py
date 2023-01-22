272941897a15053cb4bcb5e8349c48ecc118e193
Kiriill Nashchekin <kirillnashchekin@gmail.com>
2023-01-22 19:21:04 +0300

    new-greeting Ivan

--- Practise1.py
+++ Practise1.py
@@ -1,5 +1,5 @@
 print('Здравствуйте,  мои коллеги.')
-print('Кирилл привет!')
+print('Иван, привет!')
 for number in range (1, 10):
   for digit in range(1, 10):
     print( number * digit, end =' \t ') 
