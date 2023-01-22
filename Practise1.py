636996cd01cd08b64d38692134e373773b86c326
Kiriill Nashchekin <kirillnashchekin@gmail.com>
2023-01-22 19:06:03 +0300

    Revert "last changes"
    This reverts commit 9babe8c52e943467c9b07f4c82c07290219b7084.

--- Practise1.py
+++ Practise1.py
@@ -1,7 +1,7 @@
-print('Здравствуйте, друзья.')
+print('Здравствуйте,  мои коллеги.')
 
 for number in range (1, 10):
   for digit in range(1, 10):
     print( number * digit, end =' \t ') 
   print()
-  print(конец)
+  print('Безконца.')
